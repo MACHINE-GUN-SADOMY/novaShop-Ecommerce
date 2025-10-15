f1b0df1e4a7ecdaeb5cafa195d3601494f94dea4
Chainsaw Sadomator <109769365+MACHINE-GUN-SADOMY@users.noreply.github.com>
2025-10-15 20:29:23 +0100

    Coomit nuevo

--- NovaShoEcommerce/app/src/main/java/com/example/novasho_ecommerce/MainActivity.kt
+++ NovaShoEcommerce/app/src/main/java/com/example/novasho_ecommerce/MainActivity.kt
@@ -10,7 +10,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import androidx.compose.ui.Modifier 
 import com.example.novasho_ecommerce.UI.Order.OrderView
 import com.example.novasho_ecommerce.UI.Order.SimpleOrder
 
--- NovaShoEcommerce/app/src/main/java/com/example/novasho_ecommerce/UI/PaymentView/PaymentView.kt
+++ NovaShoEcommerce/app/src/main/java/com/example/novasho_ecommerce/UI/PaymentView/PaymentView.kt
@@ -1,2 +1,34 @@
 package com.example.novasho_ecommerce.UI.PaymentView
 
+import android.widget.Toast
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.TextFieldValue
+
+private val String.lenght: Any
+
+@Composable
+fun PaymentView(
+    onPaymentSuccess: () -> Unit
+){
+    var cardNumber by remember { mutableStateOf(TextFieldValue())}
+    var address by remember { mutableStateOf(TextFieldValue())}
+    var isCardValid by remember { mutableStateOf(true)}
+    var isAddressValid by remember { mutableStateOf(true)}
+    var formSubmitted by remember { mutableStateOf(false)}
+
+    fun validateForm(): Boolean {
+        val cardValid = cardNumber.text.lenght == 12
+        val addressValid = address.text.isNotEmtpy()
+        isCardValid = cardValid
+        isAddressValid = addressValid
+        return cardValid && addressValid
+    }
+
+
+}
